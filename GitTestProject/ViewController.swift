b478ce5eb1c77417a1c92ebb785e55ed367a181a
Yigitcan Lus <yigitcanlus@hotmail.com>
2019-10-19 17:19:44 +0300

    first commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Yigitcan LUS on 19.10.2019.
+//  Copyright © 2019 Yigitcan LUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print("git")
+    }
+
+   
+
+}
+
